53a464ee59d1a9f7b0b86072920644f10824132e
jgnaucke <johann.gnaucke@plusserver.com>
2023-06-07 09:49:28 +0200

    Refactor Refactor with kubebuilder instead of operator sdk Only reconcile objects if generation changed Reconcile projects every hour

--- controllers/region_controller.go
+++ controllers/region_controller.go
@@ -24,8 +24,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/pluscloudopen/reseller-cli/v2/pkg/psos"
 	"github.com/pluscloudopen/reseller-operator/api/v1alpha1"
@@ -130,7 +132,12 @@ func (r *RegionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *RegionReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	pred := predicate.GenerationChangedPredicate{}
 	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: 5,
+		}).
+		WithEventFilter(pred).
 		For(&pcov1alpha1.Region{}).
 		Complete(r)
 }
